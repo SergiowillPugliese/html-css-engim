564a65a927023eef5ca27f0eb26a1c0f159ced22
sergiowillpugliese <pugliese.sergio1987@hotmail.it>
2022-12-10 11:06:08 +0100

    update

--- sito-personale/src/app/app-routing.module.ts
+++ sito-personale/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PresentazioneComponent } from './components/presentazione/presentazione.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PresentazioneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sito-personale/src/app/components/presentazione/presentazione.component.ts
+++ sito-personale/src/app/components/presentazione/presentazione.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-presentazione',
-  templateUrl: './presentazione.component.html',
-  styleUrls: ['./presentazione.component.css']
+  templateUrl: './presentazione.component.html'
 })
 export class PresentazioneComponent implements OnInit {
 
